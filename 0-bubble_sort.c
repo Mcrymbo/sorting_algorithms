df42c44c794aa8d81b8b6b9f6fdf02b6c6ae8294
Mcrymbo <croco2023@gmail.com>
2023-09-23 07:34:00 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,17 @@
 #include "sort.h"
+/**
+ * swap - swaps integer
+ * @a: first integer
+ * @b: second integer
+ * Return: no return
+ */
+void swap( int *a, int *b)
+{
+	int temp = *a;
 
+	*a = *b;
+	*b = temp;
+}
 /**
  * bubble_sort - sorts an array in ascending order
  * @array: array of integers
@@ -8,7 +20,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int temp, swapped = 0;
+	int swapped = 0;
 	size_t i, j;
 
 	for (i = 0; i < size - 1; i++)
@@ -17,9 +29,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
 				swapped = 1;
 			}
