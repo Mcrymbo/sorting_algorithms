bf408d8ea36ed99d86692bcace4a6131500b6776
Mcrymbo <croco2023@gmail.com>
2023-09-25 16:43:06 +0000

    shell sort

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void selection_sort(int *array, size_t size);
 
 void quick_sort_hoare(int *array, size_t size);
 void sorting_hoare(int *array, int lower, int upper, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
